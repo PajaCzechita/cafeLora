7ace898a0c0966a922d8ab5e274b27b0b1a858cb
PajaCzechita <p.bajgarova@gmail.com>
2020-11-01 12:12:23 +0100

    first function for navigation menu

--- cafelora-zadani-main/src/index.js
+++ cafelora-zadani-main/src/index.js
@@ -3,9 +3,20 @@ import './style.css';
 
 console.log('funguju!');
 
+/* Menu hide/unhide */
 
 const navHamburger = document.querySelector('#nav-btn')
 navHamburger.addEventListener('click', () => {
   document.querySelector('nav').classList.toggle('nav-closed')
-
 });
+
+const navAll = document.querySelectorAll('.navbtn')
+
+navAll.forEach(nav => {
+  nav.addEventListener('click', () => {
+    const navElmMain = document.querySelector('.nav')
+    navElmMain.classList.toggle('nav-closed');
+  })
+});
+
+/* End menu */
